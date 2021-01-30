c1dd834df8ebecb50dec7dece7284d2739648df9
Feddas <feddas00@gmail.com>
2021-01-30 11:19:01 -0800

    Mia moves. Veronica talks.

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -2,22 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMover : MonoBehaviour
 {
-    public float runSpeed = 40f;
+    public float runSpeed = .4f;
     public Animator animator;
+
+    private Vector2 input;
+    private Rigidbody2D myRigidbody
+    {
+        get
+        {
+            if (_rigidbody == null)
+            {
+                _rigidbody = this.GetComponent<Rigidbody2D>();
+            }
+            return _rigidbody;
+        }
+    }
+    private Rigidbody2D _rigidbody;
 
     void Start() { }
 
     void Update()
     {
-        Vector2 input = new Vector2(
+        input = new Vector2
+        (
             Input.GetAxis("Horizontal"),
             Input.GetAxis("Vertical")
-            );
+        );
 
         animator.SetBool("IsMoving", input.magnitude > .5);
         animator.SetFloat("InputX", input.x);
         animator.SetFloat("InputY", input.y);
+    }
+
+    void FixedUpdate()
+    {
+        myRigidbody.AddForce(input * runSpeed);
     }
 }
